411946c42b2c7fd6ad12527c3435d24f0bedc394
松田健司 <ken@matsudakenjinoMacBook-Pro.local>
2018-03-01 11:45:05 +0900

    first commit

---
+++ the_most_wanted_letter.py
@@ -0,0 +1,18 @@
+import collections
+import re
+def checkio(text):
+    conv_text = re.sub(r'[^a-z]', '', text.lower())
+    count_dict = collections.Counter(conv_text)
+    return sorted(count_dict.most_common(), key=lambda x: (-x[1], x[0]))[0][0]
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio("Hello World!") == "l", "Hello test"
+    assert checkio("How do you do?") == "o", "O is most wanted"
+    assert checkio("One") == "e", "All letter only once."
+    assert checkio("Oops!") == "o", "Don't forget about lower case."
+    assert checkio("AAaooo!!!!") == "a", "Only letters."
+    assert checkio("abe") == "a", "The First."
+    print("Start the long test")
+    assert checkio("a" * 9000 + "b" * 1000) == "a", "Long."
+    print("The local tests are done.")
